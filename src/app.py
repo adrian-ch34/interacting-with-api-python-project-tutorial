77509335462b2f6c2473af969c838582e9613671
adrian-ch34 <aa2chirinosm@gmail.com>
2025-11-16 00:57:25 +0000

    Proyecto de interacción con API

--- src/app.py
+++ src/app.py
@@ -6,5 +6,36 @@ from dotenv import load_dotenv
 # load the .env file variables
 load_dotenv()
 
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# Spotify API credentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+artist_id = "3oKRxpszQKUjjaHz388fVA"
+
+results = spotify.artist_top_tracks(artist_id)
+
+songs = []
+for track in results['tracks']:
+    songs.append({
+        'name': track['name'],
+        'popularity': track['popularity'],
+        'duration_min': track['duration_ms'] / 60000
+    })
+
+
+tracks_df = pd.DataFrame(songs)
+
+print(tracks_df.head(5))
+
+plt.scatter(tracks_df['duration_min'], tracks_df['popularity'])
+plt.xlabel('Duration (minutes)')
+plt.ylabel('Popularity')
+plt.title('Relationship between duration and popularity')
+plt.show()
+
